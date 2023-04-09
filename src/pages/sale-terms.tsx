ddc12c9792ac9b166af0f72a4b51f41ed391bde7
Robin Guinant <Foohx@users.noreply.github.com>
2023-04-09 13:52:06 +0200

    fix: sale terms

--- src/pages/sale-terms.tsx
+++ src/pages/sale-terms.tsx
@@ -30,33 +30,33 @@ export default function Page() {
             <div className="space-y-4">
               <h2 className="font-medium tracking-tight text-3xl">1. Objet</h2>
               <p>
-                Les conditions générales de vente décrites ci-après détaillent
+                {`Les conditions générales de vente décrites ci-après détaillent
                 les droits et obligations de la société SARL ESPACE FRAICHEUR et
                 de son client dans le cadres de la vente des marchandises
                 suivantes : Fruits, Légumes, fruits secs, oeufs, légumineuses et
-                épicerie fine, ainsi que les compositions d'Apicius Bordeaux.
+                épicerie fine, ainsi que les compositions d'Apicius Bordeaux.`}
               </p>
               <p>
-                Toute prestations accomplie par la société SARL ESPACE FRAICHEUR
+                {`Toutes prestations accomplie par la société SARL ESPACE FRAICHEUR
                 implique donc l'adhésion sans réserve de l'acheteur aux
-                présentes conditions générales de vente.
+                présentes conditions générales de vente.`}
               </p>
             </div>
             <div className="space-y-4">
               <h2 className="font-medium tracking-tight text-3xl">2. Prix</h2>
               <p>
-                Les prix des marchandises vendues sont ceux en vigueur au jour
-                de la commande. Ils sont libellés en euros et calculés TTC.
+                {`Les prix des marchandises vendues sont ceux en vigueur au jour
+                de la commande. Ils sont libellés en euros et calculés TTC.`}
               </p>
               <p>
-                Pour les livraisons, ils seront majorés dès frais de transport
-                applicable au jour de la commande.
+                {`Pour les livraisons, ils seront majorés dès frais de transport
+                applicable au jour de la commande.`}
               </p>
               <p>
-                La société SARL ESPACE FRAICHEUR s'accorde de droit de modifier
+                {`La société SARL ESPACE FRAICHEUR s'accorde de droit de modifier
                 ses tarifs à tout moment. Toutefois, elle s'engage à facturer
                 les marchandises commandées aux prix indiqués lors de
-                l'enregistrement de la commande.
+                l'enregistrement de la commande.`}
               </p>
             </div>
             <div className="space-y-4">
@@ -64,16 +64,16 @@ export default function Page() {
                 3. Clause de rétractation
               </h2>
               <p>
-                Une fois la commande passée et validée par un mail de retour de
-                la SARL ESPACE FRAICHEUR, le client s’engage à honorer sa
+                {`Une fois la commande passée et validée par un mail de retour de
+                la SARL ESPACE FRAICHEUR, le client s'engage à honorer sa
                 commande et le paiement de sa marchandise. Par voie de
-                conséquence, il ne dispose pas de droit de rétractation.
+                conséquence, il ne dispose pas de droit de rétractation.`}
               </p>
               <p>
-                Néanmoins, si la SARL ESPACE FRAICHEUR et le client s’entendent
-                pour l’annulation de la commande et que des frais n’ont pas été
+                {`Néanmoins, si la SARL ESPACE FRAICHEUR et le client s'entendent
+                pour l'annulation de la commande et que des frais n'ont pas été
                 engagés par la société, le client pourra être dégagé de toute
-                obligation.
+                obligation.`}
               </p>
             </div>
             <div className="space-y-4">
@@ -81,7 +81,7 @@ export default function Page() {
                 4. Escompte
               </h2>
               <p>
-                Aucun escompté ne sera consenti en cas de paiement anticipé.
+                {`Aucun escompté ne sera consenti en cas de paiement anticipé.`}
               </p>
             </div>
             <div className="space-y-4">
@@ -89,20 +89,20 @@ export default function Page() {
                 5. Modalité de paiement
               </h2>
               <p>
-                Les règlement des commandes s'effectue :
+                {`Les règlement des commandes s'effectue :`}
                 <ul>
                   <li>
-                    - par chèque (sur présentation de la carte d'identité)
+                    {`- par chèque (sur présentation de la carte d'identité)`}
                   </li>
-                  <li>- par carte bancaire</li>
-                  <li>- en espèce</li>
-                  <li>- chèque Beegift</li>
+                  <li>{`- par carte bancaire`}</li>
+                  <li>{`- en espèce`}</li>
+                  <li>{`- chèque Beegift`}</li>
                 </ul>
               </p>
-              <p>Aucun autre moyen de paiement ne sera accepté.</p>
+              <p>{`Aucun autre moyen de paiement ne sera accepté.`}</p>
               <p>
-                Aucun acompte ne sera demandé, la totalité de la commande devra
-                être payée à réception ou au moment du retrait des marchandises.
+                {`Aucun acompte ne sera demandé, la totalité de la commande devra
+                être payée à réception ou au moment du retrait des marchandises.`}
               </p>
             </div>
             <div className="space-y-4">
@@ -110,7 +110,7 @@ export default function Page() {
                 6. Retrait de paiement
               </h2>
               <p>
-                En cas de défaut de paiement total ou partiel des marchandises
+                {`En cas de défaut de paiement total ou partiel des marchandises
                 livrées au jour de la réception, l'acheteur doit verser à la
                 société SARL ESPACE FRAICHEUR une pénalité de retard égale à
                 trois fois le taux de l'intérêt légal.cette pénalité est
@@ -119,7 +119,7 @@ export default function Page() {
                 demeure préalable ne soit nécessaire. En sus des indemnités de
                 retard, toute somme, non payée à sa date d'exigibilité produira
                 de plein droit le paiement d'une indemnité forfaitaire de 40
-                euros due au titre des frais de recouvrement.
+                euros due au titre des frais de recouvrement.`}
               </p>
             </div>
             <div className="space-y-4">
@@ -127,11 +127,11 @@ export default function Page() {
                 7. Clause résolutoire
               </h2>
               <p>
-                Si dans les quinze jours qui suivent la mise en œuvre delà la
+                {`Si dans les quinze jours qui suivent la mise en œuvre delà la
                 clause "retard de paiement" l'acheteur ne s'est acquitté dès
                 sommes restant dues, la vente sera résolue de plein droit et
                 pourra ouvrir à l'allocation de dommages et intérêts au profit
-                de la société SARL ESPACE FRAICHEUR.
+                de la société SARL ESPACE FRAICHEUR.`}
               </p>
             </div>
             <div className="space-y-4">
@@ -139,9 +139,9 @@ export default function Page() {
                 8. Clase de réserve de propriété
               </h2>
               <p>
-                La société SARL ESPACE FRAICHEUR conserve la propriété des biens
+                {`La société SARL ESPACE FRAICHEUR conserve la propriété des biens
                 vendus jusqu'au paiement intégral du prix, en principal et en
-                accessoires.
+                accessoires.`}
               </p>
             </div>
             <div className="space-y-4">
@@ -152,60 +152,60 @@ export default function Page() {
                 La livraison sera effectuée :
                 <ul>
                   <li>
-                    - soit par la remise direct de la marchandise à l'acquéreur
+                    {`- soit par la remise direct de la marchandise à l'acquéreur`}
                   </li>
                   <li>
-                    - soit par la mise à disposition en magasin à l'attention de
-                    l'acheteur
+                    {`- soit par la mise à disposition en magasin à l'attention de
+                    l'acheteur`}
                   </li>
                   <li>
-                    - soit au lieu indiqué par l'acheteur sur le bon de commande
+                    {`- soit au lieu indiqué par l'acheteur sur le bon de commande`}
                   </li>
                 </ul>
               </p>
               <p>
-                Les commandes faites pour livraison doivent être effectuées au
+                {`Les commandes faites pour livraison doivent être effectuées au
                 moins 24 h à l'avance. Aucune commande ne sera prise en compte
                 pour une livraison le jour même. Le délai de livraison indiqué
                 lors de l'enregistrement de la commande n'est donné qu'à titre
                 indicatif et n'est aucunement garanti. Par voie de conséquence,
                 tout retard raisonnable dans la livraison des produits ne pourra
-                pas donner lieu au profit de l'acheteur à :{" "}
+                pas donner lieu au profit de l'acheteur à : `}
                 <ul>
-                  <li>- l'allocation des dommages et intérêts</li>
-                  <li>- l'annulation de la commande</li>
+                  <li>{`- l'allocation des dommages et intérêts`}</li>
+                  <li>{`- l'annulation de la commande`}</li>
                 </ul>
               </p>
               <p>
-                Le risque du transport est supporté en totalité par l'acheteur.
+                {`Le risque du transport est supporté en totalité par l'acheteur.`}
               </p>
               <p>
-                La SARL ESPACE FRAICHEUR ayant deux points de vente, les
+                {`La SARL ESPACE FRAICHEUR ayant deux points de vente, les
                 livraisons de paniers fruits et légumes ne seront pas majorés de
-                frais de transport dans une limite raisonnable de déplacement.
+                frais de transport dans une limite raisonnable de déplacement.`}
               </p>
               <p>
-                Les livraisons de marchandises dans les communes suivantes
+                {`Les livraisons de marchandises dans les communes suivantes
                 BISCARROSSE - PARENTIS EN BORN - CESTAS - PESSAC - CANÉJAN -
                 LEOGNAN - BORDEAUX seront exonérée de frais de transport. Pour
                 les autres communes un forfait de 5 euros sera applicable pour
-                toute commande.
+                toute commande.`}
               </p>
               <p>
-                Pour les commandes et livraisons d'Apicius Bordeaux, des frais
-                de transport seront majorés au coût de la composition :
+                {`Pour les commandes et livraisons d'Apicius Bordeaux, des frais
+                de transport seront majorés au coût de la composition :`}
                 <ul>
                   <li>
-                    - livraison à 20 km autour du point de vente, livraison
-                    gratuite.
+                    {`- livraison à 20 km autour du point de vente, livraison
+                    gratuite.`}
                   </li>
                   <li>
-                    - forfait de 5 euros pour toute livraison à effectuer à plus
-                    de 20 km du point de vente et par tranche de 10 km.
+                    {`- forfait de 5 euros pour toute livraison à effectuer à plus
+                    de 20 km du point de vente et par tranche de 10 km.`}
                   </li>
                   <li>
-                    - livraison offerte pour toute commande HT de plus de 350
-                    euros.
+                    {`- livraison offerte pour toute commande HT de plus de 350
+                    euros.`}
                   </li>
                 </ul>
               </p>
@@ -215,7 +215,7 @@ export default function Page() {
                 10. Force majeure
               </h2>
               <p>
-                La responsabilité de la SARL ESPACE FRAICHEUR ne pourra être
+                {`La responsabilité de la SARL ESPACE FRAICHEUR ne pourra être
                 mise en œuvre si la non exécution ou le retard dans l'exécution
                 de l'une de ses obligations décrites dans les présentes
                 conditions générales de vente découle d'un cas de force majeure.
@@ -223,7 +223,7 @@ export default function Page() {
                 extérieur, imprévisible et irrésistible au sens de l'article
                 1148 du code civil, à cela s'ajoute tout accident de travail, et
                 absence d'employé empêchant la mise à disposition du personnel
-                nécessaire pour la réalisation des commandes.{" "}
+                nécessaire pour la réalisation des commandes. `}
               </p>
             </div>
             <div className="space-y-4">
@@ -231,10 +231,10 @@ export default function Page() {
                 11. Tribunal compétent
               </h2>
               <p>
-                Tout litige relatif à l'interprétation et à l'exécution des
+                {`Tout litige relatif à l'interprétation et à l'exécution des
                 présentes conditions générales de vente est soumis au droit
                 français. À défaut de résolution amiable, le litige sera porté
-                devant le tribunal de commerce de Bordeaux.
+                devant le tribunal de commerce de Bordeaux.`}
               </p>
             </div>
           </div>
